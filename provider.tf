ed49b7d515f558b545e8f0d911dc1725a95df643
valy-cu-i-grec <alexandruvalentinleonte@gmail.com>
2026-04-20 15:38:49 +0000

    1

--- provider.tf
+++ provider.tf
@@ -17,7 +17,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket  = "fp-2026-state-054609244415-us-east-1-an"
+    bucket  = "bucketul123123"
     key     = "terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
